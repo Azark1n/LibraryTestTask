0c06f7dc85c1e3a3cccf88f546458b7bceda261a
NR <v.azarkin@gmail.com>
2023-08-11 12:56:23 +0300

    Initial commit

---
+++ src/main/java/com/azarkin/librarytesttask/LibraryTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.azarkin.librarytesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azarkin/librarytesttask/LibraryTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azarkin.librarytesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
